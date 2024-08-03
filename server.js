2f7ff99967c9b71a4e48f70e93a1ece78edb54f3
Saurabh Pingale <saurabhpingale93@gmail.com>
2024-08-03 19:20:20 +0530

    done

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use('/api/v1/products', productRoutes);
 app.use('/api/v1/cart', cartRoutes);
 
 mongoose.connect(process.env.MONGODB_URL)
-  .then(() => console.log('DB connection successful'));
+  .then(() => console.log('Database connection successful'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
